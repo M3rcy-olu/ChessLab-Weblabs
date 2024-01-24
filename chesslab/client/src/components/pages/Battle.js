39e817ab63bf053b5c0e846de5d9fe637008abd1
Mide Togun <mide@mit.edu>
2024-01-23 19:21:55 -0500

    Battle Page w/ Navbar

--- chesslab/client/src/components/pages/Battle.js
+++ chesslab/client/src/components/pages/Battle.js
@@ -1,9 +1,15 @@
 import React from "react";
 import ChessBoard from "../modules/ChessBoard";
 import "./Battle.css";
+import NavBar from "../modules/NavBar";
 
 const Battle = () => {
-  return <ChessBoard />;
+  return (
+    <div className="Battle">
+      <ChessBoard />
+      <NavBar />
+    </div>
+  );
 };
 
 export default Battle;
