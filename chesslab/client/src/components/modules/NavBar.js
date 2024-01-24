72bcee8c8418d6d0ed5dad2b6a005e9b41c4f04c
Mide Togun <mide@mit.edu>
2024-01-23 19:17:22 -0500

    NavBar v2

--- chesslab/client/src/components/modules/NavBar.js
+++ chesslab/client/src/components/modules/NavBar.js
@@ -5,24 +5,34 @@ import "./NavBar.css";
 const NavBar = () => {
   return (
     <nav className="NavBar-Container">
-      <div className="NavBar-title">Chess Capital | Chess with Capitalism</div>
+      <div className="NavBar-Title">Chess Capital | Chess with Capitalism</div>
+
       <div className="NavBar-linkContainer">
-        <Link to="/" className="NavBar-link-home">
-          Home
+        <Link to="/" className="NavBar-Link-Home">
+          <div className="Small-Button">Home</div>
+          <div className="Small-Rectangle"></div>
         </Link>
 
-        <Link to="/Profile" className="NavBar-link">
-          Profile
+        <Link to="/Profile" className="NavBar-Link-Profile">
+          <div className="Small-Button">Profile</div>
+          <div className="Small-Rectangle"></div>
         </Link>
 
-        <Link to="/Battle" className="NavBar-link">
-          Battle
+        <Link to="/Battle" className="NavBar-Link-Battle">
+          <div className="Large-Button">Battle</div>
+          <div className="Large-Rectangle"></div>
         </Link>
 
-        <Link to="/Store" className="NavBar-link">
-          Store
+        <Link to="/Store" className="NavBar-Link-Store">
+          <div className="Large-Button">Store</div>
+          <div className="Large-Rectangle"></div>
+        </Link>
+        <Link to="/Store" className="NavBar-Link-Money">
+          <div className="Large-Button">$0</div>
+          <div className="Large-Rectangle"></div>
         </Link>
       </div>
+      <div className="NavBar-Gradient"></div>
     </nav>
   );
 };
