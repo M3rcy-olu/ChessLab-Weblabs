4dd684bb7b7ee4d3af49576448b9e2bede81d244
Jimi Oso <jimixoso@mit.edu>
2024-01-23 19:31:30 -0500

    added login

--- chesslab/client/src/components/pages/Skeleton.js
+++ chesslab/client/src/components/pages/Skeleton.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, {useState} from "react";
 import { GoogleOAuthProvider, GoogleLogin, googleLogout } from "@react-oauth/google";
-
+import { useNavigate } from "react-router-dom";
 import "../../utilities.css";
 import "./Skeleton.css";
 import ChessBoard from "../modules/ChessBoard";
@@ -26,4 +26,54 @@ const Skeleton = ({ userId, handleLogin, handleLogout }) => {
   );
 };
 
-export default Skeleton;
+const Login = (props) => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [emailError, setEmailError] = useState("")
+  const [passwordError, setPasswordError] = useState("")
+
+  const navigate = useNavigate();
+
+  const onButtonClick = () => {
+
+  }
+
+ 
+
+  return <div className={"mainContainer"}>
+    <div className={"titleContainer"}>
+      <div>Login</div>
+    </div>
+    <br />
+    <div className={"inputContainer"} >
+      <input
+          value ={email}
+          placeholder = "Enter your email here"
+          onChange = {ev => setEmail(ev.target.value)}
+          className={"inputBox"}/>
+      <label className="errorLabel">{emailError}</label>
+
+    </div>
+    <br />
+    <div className={"inputContainer"}>
+      <input
+          value={password}
+          placeholder="Enter your password here"
+          onChange={ev => setPassword(ev.target.value)}
+          className={"inputBox"} />
+      <label className="errorLabel">{passwordError}</label>
+    </div>
+    <br />
+    <div className={"inputContainer"}>
+      <input
+          className={"inputButton"}
+          type="button"
+          onClick={onButtonClick}
+          value={"Log in"} />
+    </div>
+
+  </div>
+};
+
+// export default Skeleton;
+export default Login;
